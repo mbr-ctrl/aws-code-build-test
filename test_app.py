426ff15c57e98113280e6fe2fe7c8360f321d616
Mubarak Mohamed <moubarakmohamed81@yahoo.fr>
2025-01-20 23:27:55 +0100

    Add test

---
+++ test_app.py
@@ -0,0 +1,11 @@
+import unittest
+from app import say_hello
+
+
+class TestApp(unittest.TestCase):
+    def test_say_hello(self):
+        self.assertEqual(say_hello(), "Hello World!")
+
+
+if __name__ == "__main__":
+    unittest.main()
